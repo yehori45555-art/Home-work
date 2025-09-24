61f7ffddaa8790b6cdd7f3f6ea243ebb72b06d93
yehori45555-artls <yehori4.5555@gmail.com>
2025-09-24 18:12:48 +0000

    first commit

---
+++ numb3.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cstring>
+
+   int main()
+  {
+   const char* str = "Helo, world!";
+   std::cout << "String length: " <<
+  strlen(str) << std::endl;
+  return 0;
+  }   
