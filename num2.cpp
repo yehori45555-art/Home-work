62e02a2bb8f47ba7e68ee327ca138a8218db3565
yehori45555-artls <yehori4.5555@gmail.com>
2025-09-24 17:49:53 +0000

    first commit

---
+++ num2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main()
+{
+  double number;
+   std::cout << "Enter a number:";
+  std::cin >> number;
+
+   if (number > 0 )
+  {
+  std::cout << "The number is positive." << std::endl; 
+  }
+ else if (number < 0)
+   {
+    std::cout << "The number is negative." << std::endl;
+   }
+ else 
+     {
+         std::cout << "The number is zero." << std::endl;
+     }
+    return 0;
+}
